5139018c1b85f9b03f844dde80ffb6d29092dbc6
Vinicius <viniciusgoncalves2000@hotmail.com>
2026-05-31 01:10:50 -0300

    http methods and status

---
+++ src/backend/common/http-methods.ts
@@ -0,0 +1,13 @@
+export const HttpMethod = {
+    CONNECT: "CONNECT",
+    DELETE: "DELETE",
+    GET: "GET",
+    HEAD: "HEAD",
+    OPTIONS: "OPTIONS",
+    PATCH: "PATCH",
+    POST: "POST",
+    PUT: "PUT",
+    TRACE: "TRACE",
+} as const;
+
+export type HttpMethod = typeof HttpMethod[keyof typeof HttpMethod];
---
+++ src/backend/src/http-status.ts
@@ -0,0 +1,32 @@
+export const HttpStatus = {
+    Continue: 100,
+    SwitchingProtocols: 101,
+    Processing: 102,
+
+    OK: 200,
+    Created: 201,
+    Accepted: 202,
+    NoContent: 204,
+
+    MovedPermanently: 301,
+    Found: 302,
+    NotModified: 304,
+
+    BadRequest: 400,
+    Unauthorized: 401,
+    Forbidden: 403,
+    NotFound: 404,
+    MethodNotAllowed: 405,
+    Conflict: 409,
+    Gone: 410,
+    UnprocessableEntity: 422,
+    TooManyRequests: 429,
+
+    InternalServerError: 500,
+    NotImplemented: 501,
+    BadGateway: 502,
+    ServiceUnavailable: 503,
+    GatewayTimeout: 504
+} as const;
+
+export type HttpStatus = typeof HttpStatus[keyof typeof HttpStatus];
