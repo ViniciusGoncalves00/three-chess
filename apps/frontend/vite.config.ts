20353e5f3bb7d2f4d3e32e16b9856dc9cad79349
Vinicius <viniciusgoncalves2000@hotmail.com>
2026-06-07 22:55:16 -0300

    removes unused

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
     server: {
         host: true,
@@ -22,22 +21,7 @@ export default defineConfig({
                 name: "Chess App",
                 short_name: "Chess",
                 start_url: "/",
-                display: "standalone",
-                background_color: "#111111",
-                theme_color: "#111111",
-
-                icons: [
-                    {
-                        src: "icon-192.png",
-                        sizes: "192x192",
-                        type: "image/png"
-                    },
-                    {
-                        src: "icon-512.png",
-                        sizes: "512x512",
-                        type: "image/png"
-                    }
-                ]
+                display: "standalone"
             }
         })
     ]
